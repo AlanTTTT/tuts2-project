a480d151ee7c795d25db5ca6ad4ef7b4b324bddb
Alan <allen.tang@mc900.com>
2023-11-29 10:23:18 +0800

    new update

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: './',
   css: {
     loaderOptions: {
       sass: {
         implementation: require('sass'),
         sassOptions: {
           // Enable Sass's indented syntax if you prefer
-           indentedSyntax: true
+          indentedSyntax: true,
         },
       },
     },
